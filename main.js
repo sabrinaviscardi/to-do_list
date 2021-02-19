b86ccb2284d16831f976250cf1df1c46f1ab8905
sabrinaviscardi <67836874+sabrinaviscardi@users.noreply.github.com>
2021-02-19 18:29:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+// Data store in objects
+var data = {
+    todo: [],
+    completed: []
+}
+
+// SVG files
+removeSVG =  '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"> <g> <g> <path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3 c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9 C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7 c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2 c0.6,0,1.1,0.5,1.1,1.1V7z"/> </g> <g> <g> <path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/> </g> <g> <path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z" /> </g> <g> <path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8 C14.6,17.7,14.3,18,14,18z"/> </g> </g> </g> </svg>';
+completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"> <rect class="noFill" width="22" height="22"/> <g> <path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8 c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/> </g> </svg>';                  
+
+// User clicked on the Add button
+document.getElementById('addItem').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if (value) {
+      addItem(value);
+    }
+  });
+
+// Event to add items with enter key
+document.getElementById('item').addEventListener('keydown', function (e) {
+    var value = this.value;
+    if ((e.code === 'Enter') && value) {
+        addItem(value);
+      }
+})
+
+// Add item from input
+function addItem(value){
+    addItemToDOM(value);
+    document.getElementById('item').value = '';
+  
+    data.todo.push(value);
+}
+
+// Function to remove item
+function removeItem(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
+
+    if (id === 'container__to-do'){
+        data.todo.splice(data.todo.indexOf(value), 1);
+    } else{
+        data.completed.splice(data.completed.indexOf(value), 1);
+    }
+
+    parent.removeChild(item);
+}
+
+// Function to complete or re-complete item
+function completeItem(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
+
+    if (id === 'container__to-do'){
+        data.todo.splice(data.todo.indexOf(value), 1);
+        data.completed.push(value);
+    } else{
+        data.completed.splice(data.completed.indexOf(value), 1);
+        data.todo.push(value);
+    }
+
+    // Check if it is completed or not
+    var target = (id === 'container__to-do') ? document.getElementById('container__completed'):document.getElementById('container__to-do');
+
+parent.removeChild(item);
+target.insertBefore(item, target.childNodes[0]);
+}
+
+
+// Add item to DOM from input
+function addItemToDOM(text){
+    var list = document.getElementById('container__to-do');
+
+    var item = document.createElement('li');
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('to-do__buttons');
+
+    var remove = document.createElement('button');
+    remove.classList.add('buttons__remove');
+    remove.innerHTML = removeSVG;
+
+    // Add click event for completing item
+    remove.addEventListener('click', removeItem);
+
+    var complete = document.createElement('button');
+    complete.classList.add('buttons__complete');
+    complete.innerHTML = completeSVG;
+    
+    // Add click event for removing item
+    complete.addEventListener('click', completeItem);
+
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+    list.insertBefore(item, list.childNodes[0]);
+
+}
